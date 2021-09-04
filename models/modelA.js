d7f502054d08e4e64c308454c567c57ed67b542c
Stevie Ambroise <s.ambroise@protonmail.com>
2021-09-03 19:50:20 -0600

    Add dinosaur

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var modernPterodactyl = {
+  saysThis: "RAWR",
+  alsoSaysThis: "I need to speak to the manager",
+  isTiny: true,
+  isIntimidating: true,
+  age: 17,
+  numPimples: 31,
+  eats: ["humans", "frogs", "vanishing cabinets"],
+  allergies: ["acai berries", "dandelions", "kombucha", "kefir"],
+};
