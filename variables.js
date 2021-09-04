cb97e704a96a61c6446ec6c182c451f94bea7ddf
Stevie Ambroise <s.ambroise@protonmail.com>
2021-09-03 19:39:46 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,20 @@
+var movie = "RENT";
+
+movie = "Matilda";
+
+var string = movie;
+string.length
+
+
+var isEntertaining = true;
+
+movie = "Matilda";
+
+var num4 = 7;
+
+var numb6 = 9;
+
+var product = num4 * num6;
+
+var favoriteFoods = ["Lamb Masala", "Stir Fry", "Grilled Cheese"];
+favoriteFoods.shift();
