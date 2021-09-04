13e9ec79b5cc784da0e940d29ba5369d05b530ad
Stevie Ambroise <s.ambroise@protonmail.com>
2021-09-03 19:57:48 -0600

    Add developer model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developerStevie = {
+  thinksLike: "I can do This",
+  eats: ["blueberries", "kale", "bananas"],
+  hrsStudyDaily: 5,
+  worksHard: true,
+  isGoodAt: "Self motivationß",
+  makesTimeFor: ["rest", "eating", "self care", "music"],
+  minsMeditatingDaily: 15,
+  worksFor: "Self and world betterment",
+};
